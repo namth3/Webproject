db8540786ec7b087d6e972240ae332a9618a7035
namth3 <daihiep1707@gmail.com>
2019-03-24 00:46:21 +0700

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template, session
-from flask_pymongo import PyMongo
-from werkzeug.utils import secure_filename
 from db import user
 from bson import ObjectId
 
@@ -9,32 +7,32 @@ from bson import ObjectId
 app = Flask(__name__)
 app.config
 app.config["SECRET_KEY"] = "@thanhinh1707abcdefghiklmn"
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+
+def find_username(username):
+    user_list = user.find_one({"username":username})
+    return user_list
+
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
 
-@app.route("/post")
-def new_post():
-    return render_template("post.html")
+
 
 @app.route("/signup")
 def sign_up():
     return render_template("signup.html")
 
-@app.route("/login")
-
-def find_username(username):
-    user_list = user.find({"username": username})
-    return user_list
+@app.route("/login",  methods=["GET","POST"])
 
 def login():
     if request.method == "GET":
         return render_template("login.html")
     elif request.method == "POST":
         form = request.form
-        u = form["user"]
+        u = form["username"]
         p = form["password"]
         u_list = find_username(u)
         if u_list == None:
@@ -43,16 +41,22 @@ def login():
             return "Wrong password"
         else:
             session["token"] = u
-            return  redirect("/post")
-@app.route("/logout")
+            return  redirect("/")
 
+
+@app.route("/post")
+
+def new_post():
+    return render_template("post.html")
+
+
+@app.route("/logout")
 def logout():
     del session["token"]
     return redirect("/login")
 
 
 @app.route('/uploader', methods = ['GET', 'POST'])
-
 def upload_file_page():
    return render_template('upload.html')
 
--- db.py
+++ db.py
@@ -1,7 +1,13 @@
-from flask_pymongo import PyMongo
-import app as s
+from pymongo import MongoClient
+url = "mongodb+srv://namth3:MN1Cbt6BFOvUIExD@cluster0-kondi.mongodb.net/test?retryWrites=true"
 
-s.app.config['MONGO_DBNAME'] = 'db_name'
-s.app.config['MONGO_URI'] = "mongodb+srv://namth3:MN1Cbt6BFOvUIExD@cluster0-kondi.mongodb.net/test?retryWrites=true"
+#1.connect
+client = MongoClient(url)
 
-mongo = PyMongo(s.app)
+
+#2. Get DB
+db = client.test
+
+#3. get collection
+food_collection = db["food"]
+user = db["user"]
