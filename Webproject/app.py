1b8b2963f94b3a7342f47c2ccf0f4a82c0e67759
lecaocuong198 <lecaocuong198@gmail.com>
2019-04-04 20:07:34 +0700

    fix

--- Webproject/app.py
+++ Webproject/app.py
@@ -13,44 +13,37 @@ def find_username(username):
     user_list = user_collection.find_one({"Username":username})
     return user_list
 
+locations = ["Đà Lạt","Thành Phố Hồ Chí Minh","Đà Nẵng", "Hà Nội", "Thị Trấn Sapa"]
+
 @app.route("/")
 def home_page():
-# <<<<<<< HEAD
-    
-#      return render_template("index.html")
+    # locations = ["Đà Lạt","Thành Phố Hồ Chí Minh","Đà Nẵng", "Hà Nội", "Thị Trấn Sapa"]
+    return render_template("index.html", locati = locations,a1=a1,b1=b1,c1=c1,d1=d1,e1=e1,f1=f1,g1=g1,h1=h1,a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,f2=f2,g2=g2,h2=h2)
 
-
-
-# =======
-#     locations = ["Đà Lạt","Thành Phố Hồ Chí Minh","Đà Nẵng", "Hà Nội", "Thị Trấn Sapa"]
-#     return render_template("index.html", locati = locations,a1=a1,b1=b1,c1=c1,d1=d1,e1=e1,f1=f1,g1=g1,h1=h1,a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,f2=f2,g2=g2,h2=h2)
-
-# @app.route("/image1")
-# def image1():
-#     return render_template("image1.html",a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,content1=content1)
-# @app.route("/image2")
-# def image2():
-#     return render_template("image2.html",a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,content2=content2)
-# @app.route("/image3")
-# def image3():
-#     return render_template("image3.html",a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,content3=content3)
-# @app.route("/image4")
-# def image4():
-#     return render_template("image4.html",a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,content4=content4)
-# @app.route("/image5")
-# def image5():
-#     return render_template("image5.html",a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,content5=content5)
-# @app.route("/image6")
-# def image6():
-#     return render_template("image6.html",f2=f2,content6=content6)
-# @app.route("/image7")
-# def image7():
-#     return render_template("image7.html",g2=g2,content7=content7)
-# @app.route("/image8")
-# def image8():
-#     return render_template("image8.html",h2=h2,content8=content8)
-# >>>>>>> f5fa5a29c3513251b86d4849367add7aca23e28e
-    return "Hello"
+@app.route("/image1")
+def image1():
+    return render_template("image1.html",a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,content1=content1)
+@app.route("/image2")
+def image2():
+    return render_template("image2.html",a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,content2=content2)
+@app.route("/image3")
+def image3():
+    return render_template("image3.html",a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,content3=content3)
+@app.route("/image4")
+def image4():
+    return render_template("image4.html",a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,content4=content4)
+@app.route("/image5")
+def image5():
+    return render_template("image5.html",a2=a2,b2=b2,c2=c2,d2=d2,e2=e2,content5=content5)
+@app.route("/image6")
+def image6():
+    return render_template("image6.html",f2=f2,content6=content6)
+@app.route("/image7")
+def image7():
+    return render_template("image7.html",g2=g2,content7=content7)
+@app.route("/image8")
+def image8():
+    return render_template("image8.html",h2=h2,content8=content8)
 
 @app.route("/login",  methods=["GET","POST"])
 def login():
@@ -102,12 +95,12 @@ def signup():
 @app.route("/post")
 def new_post():
     if "token" in session:
-        # if request.method == "GET":
-        #     return render_template("post.html")
-        # elif request.method == "POST":
-        #     form = request.form
-        #     n = form["place"]
-        #     p = form["content"]
+        if request.method == "GET":
+            return render_template("post.html")
+        elif request.method == "POST":
+            form = request.form
+            n = form["place"]
+            p = form["content"]
         return render_template("post.html")
     else:
         return "Please login!"
